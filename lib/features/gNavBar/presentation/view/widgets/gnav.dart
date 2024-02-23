6514f7a87d8ac2174fa41df812cba41f718f3a2d
taherelzoghby <taheramin442@gmail.com>
2024-02-23 15:16:34 +0200

    refactor gnav

--- lib/features/gNavBar/presentation/view/widgets/gnav.dart
+++ lib/features/gNavBar/presentation/view/widgets/gnav.dart
@@ -6,47 +6,56 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
-class GnavBar extends StatelessWidget {
+class GnavBar extends StatefulWidget {
   const GnavBar({super.key});
 
   @override
+  State<GnavBar> createState() => _GnavBarState();
+}
+
+class _GnavBarState extends State<GnavBar> {
+  List<GButton> tabs = [
+    GButton(
+      icon: Icons.info,
+      text: StringsEn.ourPlan.tr,
+    ),
+    GButton(
+      icon: Icons.public,
+      text: StringsEn.orderTrack.tr,
+    ),
+    GButton(
+      icon: Icons.home,
+      text: StringsEn.home.tr,
+    ),
+    GButton(
+      icon: Icons.phone,
+      text: StringsEn.connectUs.tr,
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    List<GButton> tabs = [
-      GButton(
-        icon: Icons.info,
-        text: StringsEn.ourPlan.tr,
-      ),
-      GButton(
-        icon: Icons.public,
-        text: StringsEn.orderTrack.tr,
-      ),
-      GButton(
-        icon: Icons.home,
-        text: StringsEn.home.tr,
-      ),
-      GButton(
-        icon: Icons.phone,
-        text: StringsEn.connectUs.tr,
-      ),
-    ];
     return Padding(
-      padding: EdgeInsets.symmetric(vertical: 12.h),
+      padding: AppConsts.paddingV12,
       child: GetBuilder<GnavController>(
         init: GnavController(),
         builder: (controller) {
-          return GNav(
-            selectedIndex: controller.currentIndex,
-            onTabChange: (int index) => controller.onPageChanged(index),
-            tabBorderRadius: 20.sp,
-            curve: Curves.bounceIn,
-            duration: const Duration(milliseconds: 900),
-            color: AppConsts.black,
-            activeColor: AppConsts.sWhite,
-            iconSize: 30,
-            tabBackgroundColor: AppConsts.mainColor,
-            tabs: tabs,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            textStyle: AppConsts.styleGnavText,
+          return AspectRatio(
+            aspectRatio: AppConsts.aspect16on3,
+            child: GNav(
+              selectedIndex: controller.currentIndex,
+              onTabChange: (int index) => controller.onPageChanged(index),
+              tabBorderRadius: 20.sp,
+              curve: Curves.bounceIn,
+              duration: const Duration(milliseconds: 900),
+              color: AppConsts.black,
+              activeColor: AppConsts.sWhite,
+              iconSize: 30,
+              tabBackgroundColor: AppConsts.mainColor,
+              tabs: tabs,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              textStyle: AppConsts.styleGnavText,
+            ),
           );
         },
       ),
